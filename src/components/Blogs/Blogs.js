fdc03386ccde30d763ebb6d13057f955ac0a83af
SURAJ KUMAR YADAV <thesuraj396@gmail.com>
2025-03-08 03:12:10 +0530

    Update Blogs.js

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -32,21 +32,21 @@ function Articles() {
               readLink="https://www.linkedin.com/pulse/chandrayaan-3-unveiling-triumph-truth-our-cosmic-love-yadav/?trackingId=yo4lpjOJQ7ilveNr8SA1cQ%3D%3D"
             />
           </Col>
-
-          <Col md={4} className="blog-card">
-            <BlogCard
-              imgPath={"https://media.licdn.com/dms/image/v2/D4D12AQFAUtVaSfkmOQ/article-cover_image-shrink_720_1280/article-cover_image-shrink_720_1280/0/1688414371808?e=1746662400&v=beta&t=NuQHOqtHwOMAwy3_e21fkzTLzDjU3fbzK2vPUb5jMbI"}
-              title="Vleazaar Products"
-              description="VleBazaar, founded in 2015, is an Indian online marketplace offering a wide range of products, including electronics, fashion, home & living, groceries, and local items, at affordable prices. The platform features free shipping on orders over ₹500, a 100% satisfaction guarantee, and 24/7 customer support."
-              readLink="https://www.linkedin.com/pulse/vleazaar-products-suraj-kumar-yadav/?trackingId=yo4lpjOJQ7ilveNr8SA1cQ%3D%3D"
-            />
-          </Col>
+                
           <Col md={4} className="blog-card">
             <BlogCard
               imgPath={"https://media.licdn.com/dms/image/v2/D5612AQGNYBmsfS93Ow/article-cover_image-shrink_720_1280/article-cover_image-shrink_720_1280/0/1730827229883?e=1746662400&v=beta&t=FSUWW0u284MUo7YuIP_diHZ_1yj1fnSNzwUGBQmJOt4"}
               title="The Digital Facade: How Wealthy Individuals Manipulate Public Relations in the Age of the Internet"
               description="How wealthy and influential individuals use digital public relations to reshape public perception during scandals, often obscuring the truth with curated online narratives. It examines the implications of this manipulation on young aspirants, emphasizing the importance of critical engagement and the pursuit of authenticity in a landscape where image frequently overshadows reality."
               readLink="https://www.linkedin.com/pulse/digital-facade-how-wealthy-individuals-manipulate-public-yadav-uetqc/?trackingId=asjTmL7SSEu7WfRBb3kv9Q%3D%3D"
+            />
+          </Col>
+          <Col md={4} className="blog-card">
+            <BlogCard
+              imgPath={"https://media.licdn.com/dms/image/v2/D4D12AQFAUtVaSfkmOQ/article-cover_image-shrink_720_1280/article-cover_image-shrink_720_1280/0/1688414371808?e=1746662400&v=beta&t=NuQHOqtHwOMAwy3_e21fkzTLzDjU3fbzK2vPUb5jMbI"}
+              title="Vleazaar Products"
+              description="VleBazaar, founded in 2015, is an Indian online marketplace offering a wide range of products, including electronics, fashion, home & living, groceries, and local items, at affordable prices. The platform features free shipping on orders over ₹500, a 100% satisfaction guarantee, and 24/7 customer support."
+              readLink="https://www.linkedin.com/pulse/vleazaar-products-suraj-kumar-yadav/?trackingId=yo4lpjOJQ7ilveNr8SA1cQ%3D%3D"
             />
           </Col>
         </Row>
